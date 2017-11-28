a6717543f99170de2cbd9e6d5560fcffa9c876d1
Julio Galan <ItsMonkey1@gmail.com>
2017-11-28 00:27:24 -0800

    initial commit

---
+++ src/main/java/com/galanjulio/project1/GameFrame.java
@@ -0,0 +1,218 @@
+package com.galanjulio.project1;
+
+import com.galanjulio.project1.game.Panel;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Name: Julio Galan
+ * CSID: 5685924
+ */
+public class GameFrame extends JFrame {
+
+    // Create the necessary labels (last move, treasures left, treasures found, and tries left)
+    private JLabel lastMoveLabel;
+    private JLabel treasuresLeftLabel;
+    private JLabel treasuresFoundLabel;
+    private JLabel triesLeftLabel;
+
+    // Create a List of Panels to store all necessary panels
+    private List<Panel> panels = new ArrayList<>();
+
+    // Create necessary ints (tries left, treasures left, and treasures found)
+    private int triesLeft;
+    private int treasuresLeft;
+    private int treasuresFound;
+
+    GameFrame(String title) {
+        // Let the title of the GUI be set when creating a new instance of this 'GameFrame' class
+        super(title);
+
+        // Set the size of the GUI
+        setSize(750, 350);
+
+        // Don't let people resize this GUI (messes up location of labels, buttons, and text fields)
+        setResizable(false);
+
+        // We don't need the location to be relative to anything, so we set it to null
+        setLocationRelativeTo(null);
+
+        // Set the GUI layout to null
+        setLayout(null);
+
+        // Set the default close operation to exit when it's closed
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Set the starting points for the x and y locations
+        int x = 270;
+        int y = 80;
+
+        int index = 1;
+        for (int i = 0; i < 20; i++) {
+            // Create a new panel and give it an index parameter of 'i'
+            Panel panel = new Panel(i);
+
+            // Add the newly created panel to our list
+            panels.add(panel);
+
+            // Grab the panel's button
+            JButton button = panel.getButton();
+
+            // Add an action listener for this button
+            button.addActionListener(event -> {
+
+                // Disable the button so they can't click one button more than once
+                button.setEnabled(false);
+
+                // Set the button's text to "$" if the panel is a treasure and "O" if it's not
+                button.setText(panel.isTreasure() ? "$" : "O");
+
+                // Checks if the panel is a treasure
+                if (panel.isTreasure()) {
+
+                    // Decrease the number of treasures left
+                    treasuresLeft--;
+
+                    // Increase the number of treasures found
+                    treasuresFound++;
+                }
+
+                // Decrease the number of tries left
+                triesLeft--;
+
+                // Update all labels and if the panel is a treasure set the parameter to "Treasure" else to "Miss"
+                updateLabels(panel.isTreasure() ? "Treasure" : "Miss");
+
+                // Check if they don't have any more tries
+                if (triesLeft == 0) {
+                    // Disable all buttons
+                    panels.forEach(panels -> panels.getButton().setEnabled(false));
+
+                    // Check if the amount of treasures left is greater than one
+                    if (getTreasuresLeft() > 1) {
+
+                        // They have no more tries left and there's more treasure to be found, they lost
+                        updateLabels("You lose");
+                    } else {
+
+                        // They have no more tries left and there's NO more treasure to be found, they won
+                        updateLabels("You won");
+                    }
+                }
+            });
+
+            // Set the size of the button
+            button.setSize(60, 30);
+
+            // Set the location of this button
+            button.setLocation(x, y);
+
+            // Add the panel's button to this GUI
+            add(button);
+
+            // Set the new x coordinate
+            x += 60;
+
+            // Check if the index divided by 4 is equal to 0
+            if (index % 4 == 0) {
+
+                // If the above conditional succeeds, increase the y by 30 & set the x back to 270
+                y += 30;
+                x = 270;
+            }
+
+            // Increment the index
+            index++;
+        }
+
+        // Set the default value of the treasures found (which is always 0 at first)
+        // It's not necessary to explicitly state an int to 0 (as it'll default to that) but this makes the project look cleaner
+        treasuresFound = 0;
+
+        // Set the tries left to the amount of treasures there are
+        triesLeft = getTreasuresLeft();
+
+        // Set the treasures left
+        treasuresLeft = getTreasuresLeft();
+
+        // Create the last move label
+        lastMoveLabel = new JLabel("Last move: ");
+        // Set the location of this label
+        lastMoveLabel.setLocation(270, 265);
+        // Make sure to set this invisible first since they have no moves made
+        lastMoveLabel.setVisible(false);
+        // Set the preferred size
+        lastMoveLabel.setSize(200, 10);
+        // Add the last move label to the GUI
+        add(lastMoveLabel);
+
+        // Create the treasures left label
+        treasuresLeftLabel = new JLabel("Treasures left: " + treasuresLeft);
+        // Set the location of this label
+        treasuresLeftLabel.setLocation(35, 110);
+        // Set the preferred size
+        treasuresLeftLabel.setSize(treasuresLeftLabel.getPreferredSize());
+        // Add the treasures left label to the GUI
+        add(treasuresLeftLabel);
+
+        // Create the treasures found label
+        treasuresFoundLabel = new JLabel("Treasures found: " + treasuresFound);
+        // Set the location of this label
+        treasuresFoundLabel.setLocation(35, 130);
+        // Set the preferred size
+        treasuresFoundLabel.setSize(treasuresFoundLabel.getPreferredSize());
+        // Add the treasures found label to the GUI
+        add(treasuresFoundLabel);
+
+        // Create the tries left label
+        triesLeftLabel = new JLabel("Tries left: " + triesLeft);
+        // Set the location of this label
+        triesLeftLabel.setLocation(35, 150);
+        // Set the preferred size
+        triesLeftLabel.setSize(triesLeftLabel.getPreferredSize());
+        // Add the tries left label to the GUI
+        add(triesLeftLabel);
+    }
+
+    private void updateLabels(String lastMove) {
+        // Simple method that we'll use to update a number inside of the labels
+
+        // Check if we should only add the parameter and not append "Last Move"
+        boolean onlyAddParam = lastMove.contains("won") || lastMove.contains("lose");
+
+        // First we update the last move label
+        this.lastMoveLabel.setText(onlyAddParam ? lastMove : "Last Move: " + lastMove);
+        // Set the last move label visible
+        this.lastMoveLabel.setVisible(true);
+
+        // Update the treasures left label
+        this.treasuresLeftLabel.setText("Treasures left: " + getTreasuresLeft());
+
+        // Update the treasures found label
+        this.treasuresFoundLabel.setText("Treasures found: " + treasuresFound);
+
+        // Update the tries left label
+        this.triesLeftLabel.setText("Tries left: " + triesLeft);
+    }
+
+    private int getTreasuresLeft() {
+        // Start off the amount of treasures left to 0
+        int left = 0;
+
+        // Loop through all the panels
+        for (Panel panel : panels) {
+
+            // Check if the panel is a treasure AND check if the panel's button is enabled
+            if (panel.isTreasure() && panel.getButton().isEnabled()) {
+
+                // Increment the amount of treasures left
+                left++;
+            }
+        }
+
+        // Return the amount of treasures left
+        return left;
+    }
+}
