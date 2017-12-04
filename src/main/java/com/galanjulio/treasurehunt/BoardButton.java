f3ff8348b41fb099dab3cfaa921f88424c0375cc
Julio Galan <ItsMonkey1@gmail.com>
2017-12-03 18:02:44 -0800

    Remove code that was commented out

--- src/main/java/com/galanjulio/treasurehunt/BoardButton.java
+++ src/main/java/com/galanjulio/treasurehunt/BoardButton.java
@@ -11,17 +11,9 @@ public class BoardButton extends JButton {
     // The index of this specific BoardButton
     private int index;
 
-    // Boolean variable to check if this panel contains a treasure
-    // private boolean treasure;
-
     public BoardButton(int index) {
         // Set the index to the one passed from the constructor
         this.index = index;
-
-        // 50/50 chance of this panel containing a treasure
-        // This is assigned from the static final random instance from the main class
-        // this.treasure = Main.RANDOM.nextBoolean();
-
         // Name the button
         setText("???");
 
@@ -39,11 +31,4 @@ public class BoardButton extends JButton {
         // Return this panel's index
         return index;
     }
-
-    /*
-    public boolean isTreasure() {
-        // Return weather or not this panel is a treasure
-        return treasure;
-    }
-    */
 }
